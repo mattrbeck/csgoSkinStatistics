e19cb69326003294aaf055152a080eb8e1715899
Matthew Beck <me@mattrb.com>
2026-06-25 11:45:04 +0200

    Extract SteamAccountManager into its own file

--- Program.cs
+++ Program.cs
@@ -711,35 +711,4 @@ namespace CSGOSkinAPI.Services
         public static void SetWrappedStickerId(CEconItemPreviewDataBlock.Sticker sticker, uint id)
             => Extensible.AppendValue<uint>(sticker, WrappedStickerTag, id);
     }
-
-    public class SteamAccountManager
-    {
-        public SteamClient Client { get; }
-        public SteamUser User { get; }
-        public SteamGameCoordinator GC { get; }
-        public CallbackManager Manager { get; }
-        public SteamAccount Account { get; }
-        public bool IsConnected { get; set; }
-        public bool IsLoggedIn { get; set; }
-        // Whether the in-flight logon used a cached refresh token, so a rejection can fall back
-        // to a fresh credential auth (see SteamService.OnLoggedOn).
-        public bool UsedCachedToken { get; set; }
-        public DateTime LastRequestTime { get; set; } = DateTime.MinValue;
-        public SemaphoreSlim RateLimitSemaphore { get; } = new(1, 1);
-
-        public SteamAccountManager(SteamAccount account)
-        {
-            Account = account;
-            Client = new SteamClient();
-            User = Client.GetHandler<SteamUser>()!;
-            GC = Client.GetHandler<SteamGameCoordinator>()!;
-            Manager = new CallbackManager(Client);
-        }
-
-        public void Dispose()
-        {
-            RateLimitSemaphore?.Dispose();
-            Client?.Disconnect();
-        }
-    }
 }
